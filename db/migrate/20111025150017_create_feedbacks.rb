6336c3ba9dead500acb7ffc1de1f394599eb137e
Jason <jason@Jason-PC-Ubuntu.(none)>
2011-12-22 11:49:29 +0800

    add some files for version 2

---
+++ db/migrate/20111025150017_create_feedbacks.rb
@@ -0,0 +1,16 @@
+class CreateFeedbacks < ActiveRecord::Migration
+  def self.up
+    create_table :feedbacks do |t|
+      t.string :title
+      t.text :content
+      t.string :email
+      t.string :user_name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :feedbacks
+  end
+end
