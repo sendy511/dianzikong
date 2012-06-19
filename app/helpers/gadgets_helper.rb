5121f152b021557a265f2323f3252ce5e3e12bd7
Jason <jason@Jason-PC-Ubuntu.(none)>
2012-06-19 22:54:13 +0800

    remove the html tag for summary of index page.

--- app/helpers/gadgets_helper.rb
+++ app/helpers/gadgets_helper.rb
@@ -1,2 +1,21 @@
 module GadgetsHelper
+  def cleanup_html(src)
+    #remove all html tags except the image
+    src = src.gsub(/<.*?>/) do |matched|
+      p matched
+      if matched.downcase.include?("/image") then
+        "</image>"
+      elsif matched.downcase.include?("image") then
+        "<image>"
+      else
+        ""
+      end
+    end
+    #Remove the image, if there is no end image tag
+    if src.downcase.include?("image") && !src.downcase.include?("/image")
+      src = src.slice(0, src.downcase.index("<image>"))
+    end
+    return src
+  end
 end
+
