57c9f70cda4b6363c465f7f2d0cf90b845779f6a
Jason <jason@Jason-PC-Ubuntu.(none)>
2012-06-21 23:41:26 +0800

    fix bug: remove <img /> wrongly.

--- app/helpers/gadgets_helper.rb
+++ app/helpers/gadgets_helper.rb
@@ -1,19 +1,41 @@
 module GadgetsHelper
   def cleanup_html(src)
+    src = src.downcase
+
     #remove all html tags except the image
     src = src.gsub(/<.*?>/) do |matched|
+      #Test: print matched
       p matched
-      if matched.downcase.include?("</img>") || matched.downcase.include?("<img") then
-        matched.downcase
+      # Keep the img tags
+      if matched.include?("</img>") || matched.include?("<img") then
+        matched
       else
         ""
       end
     end
+
     #Remove the image, if there is no end image tag
-    if src.downcase.include?("<img") && !src.downcase.include?("</img>")
-      src = src.slice(0, src.downcase.index("<img"))
+    indx = src.rindex("<img")
+    # No Img tag
+    if indx == -1 then
+      rtn =  src
+    else
+      p indx
+      afterImgSrc = src.slice(indx, src.length - indx)
+      p afterImgSrc
+      # Have complete <img /> or <img> </img>
+      p /<img[^>]*\/>/ =~ afterImgSrc 
+      if /<img[^>]*\/>/ =~ afterImgSrc || afterImgSrc.include?("</img>") then
+        rtn = src
+      else
+        rtn = src.slice(0, indx)
+      end
     end
-    return src
+
+    p "----------------"
+    p rtn 
+    p "----------------"
+    return rtn 
   end
 end
 
--- test/unit/helpers/gadgets_helper_test.rb
+++ test/unit/helpers/gadgets_helper_test.rb
@@ -15,4 +15,29 @@ class GadgetsHelperTest < ActionView::TestCase
     Rails::logger.debug processed
     assert expect == processed
   end
+
+  test "cleaup_html_1" do
+  org = %q|<p style="text-align:left;">
+    <p>&nbsp;</p>
+    <div id="artibody" class="blkContainerSblkCon">
+    <div class="img_wrapper">&nbsp;</div>
+    <div class="img_wrapper">&nbsp;</div>
+    <div class="img_wrapper"><a target="_blank" href="http://slide.tech.sina.com.cn/notebook/slide_5_32294_22305.html">微软发布Surface平板电脑</a></div>
+    <div class="img_wrapper"><a target="_blank" href="http://slide.tech.sina.com.cn/notebook/slide_5_32294_22305.html"><img title="微软发布Surface平板电脑 再度挑战苹果" src="http://i1.sinaimg.cn/IT/2012/0619/U5388P2DT20120619072418.jpg" alt="微软发布Surface平板电脑 再度挑战苹果" /></a><span class="img_descr">微软发布Surface平板电脑 再度挑战苹果</span></div>
+    </div>
+    </p>|
+expect= %q|
+    &nbsp;
+    
+    &nbsp;
+    &nbsp;
+    微软发布Surface平板电脑
+    <img title="微软发布Surface平板电脑 再度挑战苹果" src="http://i1.sinaimg.cn/IT/2012/0619/U5388P2DT20120619072418.jpg" alt="微软发布Surface平板电脑 再度挑战苹果" />微软发布Surface平板电脑 再度挑战苹果
+    
+   |
+    processed = cleanup_html(org)
+    Rails::logger.debug processed
+    Rails::logger.debug expect 
+    assert expect == processed
+  end
 end
