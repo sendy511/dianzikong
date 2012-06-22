f0cf03195768b225f7dbc40b732b52f8949026ae
Jason <jason@Jason-PC-Ubuntu.(none)>
2012-06-22 22:47:56 +0800

    fix bug: <img> will be convert to downcase, which cause can not find the resource.

--- app/helpers/gadgets_helper.rb
+++ app/helpers/gadgets_helper.rb
@@ -1,13 +1,12 @@
 module GadgetsHelper
   def cleanup_html(src)
-    src = src.downcase
 
     #remove all html tags except the image
     src = src.gsub(/<.*?>/) do |matched|
       #Test: print matched
       p matched
       # Keep the img tags
-      if matched.include?("</img>") || matched.include?("<img") then
+      if matched.downcase.include?("</img>") || matched.downcase.include?("<img") then
         matched
       else
         ""
@@ -15,7 +14,7 @@ module GadgetsHelper
     end
 
     #Remove the image, if there is no end image tag
-    indx = src.rindex("<img")
+    indx = src.downcase.rindex("<img")
     # No Img tag
     if indx == nil then
       rtn =  src
@@ -24,8 +23,8 @@ module GadgetsHelper
       afterImgSrc = src.slice(indx, src.length - indx)
       p afterImgSrc
       # Have complete <img /> or <img> </img>
-      p /<img[^>]*\/>/ =~ afterImgSrc 
-      if /<img[^>]*\/>/ =~ afterImgSrc || afterImgSrc.include?("</img>") then
+      p /<img[^>]*?\/>/ =~ afterImgSrc.downcase 
+      if /<img[^>]*?\/>/ =~ afterImgSrc.downcase || afterImgSrc.downcase.include?("</img>") then
         rtn = src
       else
         rtn = src.slice(0, indx)
