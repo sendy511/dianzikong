81bef5f88bd270a4287d3e8d92d60fe043c03a16
Jason <jason@Jason-PC-Ubuntu.(none)>
2013-05-26 18:38:12 +0800

    upgrade to rails2

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '6e398785f003d632acde18561fedf669da3727421b232a847052f6a13522943152c1f71c23c209dd72bdb70a2a983023203494e4b0425038c307eab5277b82a7';
+ActionController::Base.config.secret_token = '6e398785f003d632acde18561fedf669da3727421b232a847052f6a13522943152c1f71c23c209dd72bdb70a2a983023203494e4b0425038c307eab5277b82a7';
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DianziKong::Application.config.secret_token = 'edab257988840ba8219b9f5a75511eda6bb35f3b6b56820e882564bf6e89262b6a98b9b8ddf45a33d6cd44fde2e1fbba102602982ffbadd0819058b9ee2dd832'
