cce4c7cf9fc1b910733abde59f9c0491092da0c2
Jason Dou <douyanseng@163.com>
2013-12-24 22:49:14 +0800

    return the previous page after sign in, 2. igore ckeditor pic storage folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def check_authentication  
+    #Store Previous url
+    #session[:retur_to] = request.url
+    
+    store_location
+
      unless session[:user]  
-       session[:intended_action] = action_name  
        redirect_to :action => :signin_form  
      end  
    end  
    
    def signin 
-     session[:user] = AdminUser.authenticate(params[:Gadget][:name], params[:Gadget][:password])
-     #session[:user] = AdminUser.authenticate("","")
-     redirect_to :action => session[:intended_action]  
+    session[:user] = AdminUser.authenticate(params[:Gadget][:name], params[:Gadget][:password])
+    #session[:user] = AdminUser.authenticate("","")
+    redirect_back_or_default
+    #redirect_to :action => session[:intended_action]  
    end  
    
    def signout  
@@ -22,5 +27,16 @@ class ApplicationController < ActionController::Base
    def signin_form
       render(:layout => false)     
    end
+ def store_location
+   session[:return_to] = if request.get?
+     request.url
+    else
+     request.referer
+    end
+ end
+ 
+ def redirect_back_or_default
+    redirect_to(session.delete(:return_to) || request.refer || default)
+ end
 
 end
